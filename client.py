02ceee32cdedaaf21c093ec3d3fc34787f106aad
harithfakhiri <13519161@std.stei.itb.ac.id>
2021-11-23 22:28:19 +0700

    Co-authored-by: ibnuhafizzh <ibnuhafizzh@users.noreply.github.com> Co-authored-by: syarifahaisha <syarifahaisha@users.noreply.github.com>

--- client.py
+++ client.py
@@ -15,18 +15,42 @@
 # client.bind(("", 37020))
 # while True:
 #     data, addr = client.recvfrom(1024)
-#     print("received message: %s"%data)
+#     print("received message: %s" %data)
+
+# import socket
+
+# UDP_IP = "127.0.0.1"
+# UDP_PORT = 5005
+# MESSAGE = b"Hello, World!"
+
+# print("UDP target IP: %s" % UDP_IP)
+# print("UDP target port: %s" % UDP_PORT)
+# print("message: %s" % MESSAGE)
+
+# sock = socket.socket(socket.AF_INET, # Internet
+#                      socket.SOCK_DGRAM) # UDP
+# sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 
 import socket
+import time
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
-MESSAGE = b"Hello, World!"
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 
-print("UDP target IP: %s" % UDP_IP)
-print("UDP target port: %s" % UDP_PORT)
-print("message: %s" % MESSAGE)
+# Enable port reusage so we will be able to run multiple clients and servers on single (host, port). 
+# Do not use socket.SO_REUSEADDR except you using linux(kernel<3.9): goto https://stackoverflow.com/questions/14388706/how-do-so-reuseaddr-and-so-reuseport-differ for more information.
+# For linux hosts all sockets that want to share the same address and port combination must belong to processes that share the same effective user ID!
+# So, on linux(kernel>=3.9) you have to run multiple servers and clients under one user to share the same (host, port).
+# Thanks to @stevenreddie
+client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+# Enable broadcasting mode
+client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+# Set a timeout so the socket does not block
+# indefinitely when trying to receive data.
+client.settimeout(0.2)
+message = b"your very important message"
+while True:
+    client.sendto(message, ('<broadcast>', 37020))
+    print("message sent!")
+    time.sleep(1)
--- server.py
+++ server.py
@@ -22,15 +22,34 @@
 #     print("message sent!")
 #     time.sleep(1)
 
+# import socket
+
+# UDP_IP = "127.0.0.1"
+# UDP_PORT = 5005
+ 
+# sock = socket.socket(socket.AF_INET, # Internet
+#                socket.SOCK_DGRAM) # UDP
+# sock.bind((UDP_IP, UDP_PORT))
+ 
+# while True:
+#     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+#     print("received message: %s" %data)
+
 import socket
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
- 
-sock = socket.socket(socket.AF_INET, # Internet
-               socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
- 
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) # UDP
+
+# Enable port reusage so we will be able to run multiple clients and servers on single (host, port). 
+# Do not use socket.SO_REUSEADDR except you using linux(kernel<3.9): goto https://stackoverflow.com/questions/14388706/how-do-so-reuseaddr-and-so-reuseport-differ for more information.
+# For linux hosts all sockets that want to share the same address and port combination must belong to processes that share the same effective user ID!
+# So, on linux(kernel>=3.9) you have to run multiple servers and clients under one user to share the same (host, port).
+
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# Enable broadcasting mode
+server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+server.bind(("", 37020))
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print("received message: %s" %data)
+    data, addr = server.recvfrom(1024)
+    print("received message: %s"%data)
