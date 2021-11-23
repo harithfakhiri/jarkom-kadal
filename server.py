58e8d6200ffc3c7327a17a7c61e22942beed646a
harithfakhiri <13519161@std.stei.itb.ac.id>
2021-11-23 22:13:17 +0700

    adad

--- server.py
+++ server.py
@@ -24,13 +24,13 @@
 
 import socket
 
- UDP_IP = "127.0.0.1"
- UDP_PORT = 5005
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
  
- sock = socket.socket(socket.AF_INET, # Internet
+sock = socket.socket(socket.AF_INET, # Internet
                socket.SOCK_DGRAM) # UDP
- sock.bind((UDP_IP, UDP_PORT))
+sock.bind((UDP_IP, UDP_PORT))
  
-  while True:
+while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print("received message: %s" % data)
+    print("received message: %s" %data)
